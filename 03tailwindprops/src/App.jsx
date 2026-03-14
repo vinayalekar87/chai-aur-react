3b8352f8fd392d6963a721f62e11eeea1a992438
valekar-fms <calekav@jci.com>
2026-03-14 10:17:28 +0530

    set callback function interview question

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -12,11 +12,33 @@ function App() {
   city: "Pune"
   }
 
+  const addValue = () => {
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const removeValue = () => {
+    setCount(prevCount => prevCount - 1);
+    setCount(prevCount => prevCount - 1);
+    setCount(prevCount => prevCount - 1);
+    setCount(prevCount => prevCount - 1);
+  }
+
+
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-blue-500">Tailwind test</h1>
       <Card username="Channel Vinay" someprop={myObject}/>
       <Card username="Channel Vedansh"/>
+      <button onClick={addValue} className="px-4 py-2 bg-green-500 text-white rounded-lg mt-4">
+        Count: {count}
+      </button>
+      <button onClick={removeValue} className="px-4 py-2 bg-red-500 text-white rounded-lg mt-4 ml-2">
+        Decrease Count
+      </button>
+      <p>footer: {count}  </p>
     </>
   )
 }
